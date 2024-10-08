c70c15a841c389239a7ec7d47a086800f97ee7ab
Davide Zarantonello <davide@zarantonello.dev>
2024-10-08 14:48:48 +0000

    chore: fix more lints

--- src/0001/solution.h
+++ src/0001/solution.h
@@ -3,6 +3,6 @@
 #ifndef SRC_0001_SOLUTION_H_
 #define SRC_0001_SOLUTION_H_
 
-int* two_sum(int* nums, int nums_size, int target, int* return_size);
+int* two_sum(const int* nums, int nums_size, int target, int* return_size);
 
 #endif  // SRC_0001_SOLUTION_H_
